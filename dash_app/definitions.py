c9de29f1020882f11c4304cf22a315f6ea8f9bc1
Bernat, Anastasia <anastasia.bernat@pnnl.gov>
2025-01-11 16:33:14 -0800

    Deployment test 12.

--- dash_app/definitions.py
+++ dash_app/definitions.py
@@ -3,6 +3,9 @@
 
 import os
 
+# DEV: https://gridcerf.dev.msdlive.org/
+# PROD: https://gridcerf.msdlive.org/
+
 # CLIENT (BROWSER) PATHS
 PORT = int(os.environ.get("PORT", 8060))
 REQUESETS_PATHNAME_PREFIX = "/"
@@ -14,9 +17,7 @@ CONNECT_TO_LAMBDA = True
 
 if CONNECT_TO_LAMBDA:
 
-    print("DEPLOYMENT 11")
-
-    # https://gridcerf.dev.msdlive.org/
+    print("DEPLOYMENT 12")
 
     DATASET_ID = "1ffea-emt93" # MSD-LIVE added dataset id that goes to DEV
     DATA_DIR = ""
@@ -26,7 +27,7 @@ if CONNECT_TO_LAMBDA:
         print(" ********** ", "LAMBDA_TASK_ROOT is None")
         METADATA_DIR = './metadata'
     else:
-        print("********** ", os.getenv('LAMBDA_TASK_ROOT'))
+        print("********** LAMBDA_TASK_ROOT is ", os.getenv('LAMBDA_TASK_ROOT'))
         METADATA_DIR = os.path.join(LAMBDA_TASK_ROOT, "dash_app", "metadata")
 
 else:
