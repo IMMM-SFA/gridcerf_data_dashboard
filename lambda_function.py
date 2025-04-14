a138b674624ac3189363fe01439767c3508de471
Travis Thurber <thurber@users.noreply.github.com>
2025-04-14 09:43:35 -0700

    init module

--- lambda_function.py
+++ lambda_function.py
@@ -9,7 +9,7 @@ from apig_wsgi import make_lambda_handler
 # SOURCED SCRIPT
 # from dash_app.app import app
 # from dash_app.layout import create_app
-from test_app.test_app import create_app
+from dash_app.test_app import create_app
 
 @lru_cache(maxsize=5)
 def build_handler(url_prefix: str) -> "Dash":
